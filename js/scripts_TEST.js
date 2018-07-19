e0fb323210cf1f5b16f2b74e48b0e85154130512
Nick Noel <nicknoel251289@gmail.com>
2018-07-19 16:51:09 -0400

    updaed tabs to display effectively when hovered

--- js/scripts_TEST.js
+++ js/scripts_TEST.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
     document.getElementById('work_button').style.display = "none";
     document.getElementById('artwork_auto_scroll').style.display = "block";
     document.getElementById('section_2').style.display = "block";
+    document.getElementById('right-tab').style.display = "inline-flex";
   });
 
   document.getElementById('work_button').addEventListener('click', function(){
@@ -18,15 +19,16 @@ $(document).ready(function(){
     document.getElementById('view_button').style.display = "none";
     document.getElementById('case_studies_auto_scroll').style.display = "block";
     document.getElementById('section_3').style.display = "block";
+    document.getElementById('left-tab').style.display = "inline-flex";
   });
 
   document.getElementById('left-tab').addEventListener('click', function(){
-    document.getElementById('left-tab').style.visibility = "hidden";
-    document.getElementById('left_block').style.width = "100%";
-    document.getElementById('right_block').style.width = "0%";
+    document.getElementById('left-tab').style.display = "none";
+    document.getElementById('right-tab').style.display = "inline-flex";
     document.getElementById('right-tab').style.zIndex = "0";
     document.getElementById('left-tab').style.zIndex = "300";
-    document.getElementById('right-tab').style.visibility = "visible";
+    document.getElementById('left_block').style.width = "100%";
+    document.getElementById('right_block').style.width = "0%";
     document.getElementById('view_button').style.display = "none";
     document.getElementById('work_button').style.display = "none";
     document.getElementById('case_studies_auto_scroll').style.display = "none";
@@ -35,13 +37,19 @@ $(document).ready(function(){
     document.getElementById('section_3').style.display = "none";
   });
 
+  $("#left-tab").hover(function(){
+    $("#left-tab-div").css("visibility", "visible");
+    }, function(){
+    $("#left-tab-div").css("visibility", "hidden");
+  });
+
   document.getElementById('right-tab').addEventListener('click', function(){
-    document.getElementById('right-tab').style.visibility = "hidden";
-    document.getElementById('right_block').style.width = "100%";
-    document.getElementById('left_block').style.width = "0%";
+    document.getElementById('right-tab').style.display = "none";
+    document.getElementById('left-tab').style.display = "inline-flex";
     document.getElementById('left-tab').style.zIndex = "0";
     document.getElementById('right-tab').style.zIndex = "300";
-    document.getElementById('left-tab').style.visibility = "visible";
+    document.getElementById('right_block').style.width = "100%";
+    document.getElementById('left_block').style.width = "0%";
     document.getElementById('work_button').style.display = "none";
     document.getElementById('view_button').style.display = "none";
     document.getElementById('artwork_auto_scroll').style.display = "none";
@@ -50,7 +58,11 @@ $(document).ready(function(){
     document.getElementById('section_3').style.display = "block";
   });
 
-
+  $("#right-tab").hover(function(){
+    $("#right-tab-div").css("visibility", "visible");
+    }, function(){
+    $("#right-tab-div").css("visibility", "hidden");
+  });
 
   $("#artwork_auto_scroll").on('click', function(event) {
     // Makes sure this.hash has a value before overriding default behavior

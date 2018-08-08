7fed1a065214707da912e440904058f77a33b4f1
Nick Noel <nicknoel251289@gmail.com>
2018-08-08 17:49:16 -0400

    got filter working as wanted

--- js/scripts_TEST.js
+++ js/scripts_TEST.js
@@ -169,47 +169,121 @@ $(document).ready(function(){
       }
     });*/
 
+    /* BEST ONE SO FAR
     $('#charcoal_filter').click(function(e) {
       if(document.getElementById("charcoal_filter").checked == true){
         $('.charcoal').animate({width: 'show'}, 600);
+        if ($("input:checkbox:not(:checked)")){
+          $('.card').not(".charcoal").animate({width: 'hide'}, 600);
+        }
       } else if(document.getElementById("charcoal_filter").checked == false){
         $('.charcoal').animate({width: 'hide'}, 600);
       }
-    });
-    $('#pencil_filter').click(function(e) {
-      if(document.getElementById("pencil_filter").checked == true){
-        $('.pencil').animate({width: 'show'}, 600);
-      } else if(document.getElementById("charcoal_filter").checked == false){
-        $('.pencil').animate({width: 'hide'}, 600);
-      }
-    });
-
-    /*$("label").click(function() {
-      if($(this).is("#charcoal_button")) {
-        $('.card').not('.charcoal').toggle({width: 'hide'}, 600);
-        $("#charcoal_button").toggleClass("clicked");
-      } else if($(this).is("#pencil_button")) {
-        $('.card').not('.pencil').toggle({width: 'hide'}, 600);
-        $("#pencil_button").toggleClass("clicked");
-      }
     });*/
 
-    /* $('#charcoal_filter').click(function(e) {
-      if(document.getElementById("charcoal_filter").checked == true){
-        $('.charcoal').animate({width: 'toggle'});
-        $('.charcoal img').animate({width: 'toggle'});
-        $('.art').css('justify-content', 'flex-start');
-      }else if(document.getElementById("charcoal_filter").checked == false){
-        $('.art').css('justify-content', 'space-between');
-        $('.charcoal').slideToggle({visibility: 'visible'});
-        $('.charcoal img').animate({width: 'toggle'});
-      }
-    }); */
 
-    /*$('#charcoal_filter').on('click','div.charcoal', function(e) {
-      if(document.getElementById("charcoal_filter").checked == true){
-        $(this).css({"visibility":"hidden",display:'block'}).slideUp();
-      }
-    });*/
+    var filter = document.getElementsByClassName('filter_button');
+    $("#charcoal_button").click(function(){
+        if($("#charcoal_button").hasClass('clicked')){
+          $("#charcoal_button").removeClass('clicked');
+          $("#charcoal").removeClass('show');
+          $(".charcoal").animate({width: 'hide'}, 600);
+        }else if(!$("#charcoal_button").hasClass('clicked')){
+          $("#charcoal_button").addClass('clicked');
+          $(".charcoal").addClass('show');
+          $(".charcoal").animate({width: 'show'}, 600);
+        }
+        for(var i=0; i<filter.length; i++){
+          if(!$(filter[i]).hasClass('clicked')){
+            $(".card").not(".show").animate({width: 'hide'}, 600);
+          }
+        }
+    });
+
+    $("#pencil_button").click(function(){
+        if($("#pencil_button").hasClass('clicked')){
+          $("#pencil_button").removeClass('clicked');
+          $(".pencil").removeClass('show');
+          $(".pencil").animate({width: 'hide'}, 600);
+        }else if(!$("pencil_button").hasClass('clicked')){
+          $("#pencil_button").addClass('clicked');
+          $(".pencil").addClass('show');
+          $(".pencil").animate({width: 'show'}, 600);
+        }
+        for(var j=0; j<filter.length; j++){
+          if(!$(filter[j]).hasClass('clicked')){
+            $(".card").not(".show").animate({width: 'hide'}, 600);
+          }
+        }
+    });
+
+    $("#logo_button").click(function(){
+        if($("#logo_button").hasClass('clicked')){
+          $("#logo_button").removeClass('clicked');
+          $(".logo").removeClass('show');
+          $(".logo").animate({width: 'hide'}, 600);
+        }else if(!$("logo_button").hasClass('clicked')){
+          $("#logo_button").addClass('clicked');
+          $(".logo").addClass('show');
+          $(".logo").animate({width: 'show'}, 600);
+        }
+        for(var k=0; k<filter.length; k++){
+          if(!$(filter[k]).hasClass('clicked')){
+            $(".card").not(".show").animate({width: 'hide'}, 600);
+          }
+        }
+    });
+
+    $("#mockup_button").click(function(){
+        if($("#mockup_button").hasClass('clicked')){
+          $("#mockup_button").removeClass('clicked');
+          $(".mockup").removeClass('show');
+          $(".mockup").animate({width: 'hide'}, 600);
+        }else if(!$("mockup_button").hasClass('clicked')){
+          $("#mockup_button").addClass('clicked');
+          $(".mockup").addClass('show');
+          $(".mockup").animate({width: 'show'}, 600);
+        }
+        for(var k=0; k<filter.length; k++){
+          if(!$(filter[k]).hasClass('clicked')){
+            $(".card").not(".show").animate({width: 'hide'}, 600);
+          }
+        }
+    });
+
+    $("#graphic_button").click(function(){
+        if($("#graphic_button").hasClass('clicked')){
+          $("#graphic_button").removeClass('clicked');
+          $(".graphic").removeClass('show');
+          $(".graphic").animate({width: 'hide'}, 600);
+        }else if(!$("graphic_button").hasClass('clicked')){
+          $("#graphic_button").addClass('clicked');
+          $(".graphic").addClass('show');
+          $(".graphic").animate({width: 'show'}, 600);
+        }
+        for(var k=0; k<filter.length; k++){
+          if(!$(filter[k]).hasClass('clicked')){
+            $(".card").not(".show").animate({width: 'hide'}, 600);
+          }
+        }
+    });
+
+    $("#misc_button").click(function(){
+        if($("#misc_button").hasClass('clicked')){
+          $("#misc_button").removeClass('clicked');
+          $(".misc").removeClass('show');
+          $(".misc").animate({width: 'hide'}, 600);
+        }else if(!$("misc_button").hasClass('clicked')){
+          $("#misc_button").addClass('clicked');
+          $(".misc").addClass('show');
+          $(".misc").animate({width: 'show'}, 600);
+        }
+        for(var k=0; k<filter.length; k++){
+          if(!$(filter[k]).hasClass('clicked')){
+            $(".card").not(".show").animate({width: 'hide'}, 600);
+          }
+        }
+    });
+
 
 });
